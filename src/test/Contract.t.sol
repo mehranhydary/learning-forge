4045d320dc49d4751e3bba0ef21a57fd4deaa8ab
mehran <mehran.hydary@gmail.com>
2022-01-16 00:14:30 -0500

    First test

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
+// Forge will help me learn how to write tests in Solidity
+
+// DS Test gives my test contract access to testing functions and events (from ds-test/test.sol).
+// An example of this is the assertTrue function being used in DSTest. There's other types of assert
+// functions available as well.
+
 import "ds-test/test.sol";
 
 contract ContractTest is DSTest {
-    function setUp() public {}
-
+    // This is a special fucntion that will be called before any tests are run
+    uint a;
+    
+    function setUp() public {
+        a = 10;
+    }
+    // Each test you create will be a function
     function testExample() public {
-        assertTrue(true);
+        // assertEq can be found in ds-test/test.sol
+        assertEq(a, 10);
     }
 }
