4545170cd4cafec4f295b554ca733eac381ff11d
mehran <mehran.hydary@gmail.com>
2022-01-16 00:31:54 -0500

    Added a simple function and a simple test

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-contract Contract {}
+contract Contract {
+  function addOne(uint256 x) external returns (uint256) {
+    return ++x;
+  }
+}
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -9,25 +9,44 @@ pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
 
-contract ContractTest is DSTest {
-    // This is a special fucntion that will be called before any tests are run
-    uint a;
-    
-    function setUp() public {
-        a = 10;
-    }
-    
-    // Each test you create will be a function
-    // For the test to work, it must contain the name test (if it was just called example, 
-    // forge test will not run it)
-    
-    function testExample() public {
-        // assertEq can be found in ds-test/test.sol
-        assertEq(a, 10);
-    }
+// Import your contract(s)
+import "../Contract.sol";
 
-    // If I want to create a test that will fail, I can do this (add Fail to the function name)
-    function testFailExample() public {
-        assertEq(a, 9); // This shows up as a pass
-    }
+contract ContractTest is DSTest {
+  // Real:
+  Contract myContract;
+  
+  function setUp() public {
+    myContract = new Contract();
+  }
+
+  function testAddOne() public {
+    assertEq(3, myContract.addOne(2));
+  }
+
+  // Starting off: 
+
+  // // This is a special fucntion that will be called before any tests are run
+  // uint a;
+  
+  // function setUp() public {
+  //     a = 10;
+  // }
+  
+  // // Each test you create will be a function
+  // // For the test to work, it must contain the name test (if it was just called example, 
+  // // forge test will not run it)
+  
+  // function testExample() public {
+  //     // assertEq can be found in ds-test/test.sol
+  //     assertEq(a, 10);
+  // }
+
+  // // If I want to create a test that will fail, I can do this (add Fail to the function name)
+  // // May not use this as often because it's kind of weird
+  // function testFailExample() public {
+  //     assertEq(a, 9); // This shows up as a pass
+  // }
+
+  // // Another thing we can do is use a function called "expectRevert"; when that passes, it's ok
 }
